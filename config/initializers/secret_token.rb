3421645c58534a5c7c1dea1af478db25ef290270
Tom Dyer <tdyer@ourstage.com>
2014-03-12 23:22:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecidivismVisualizer::Application.config.secret_key_base = 'cb03d1acc55481e8c5610f76471c0abdac96df525020eb75f8b301c212ef2f76deb14053a44f2e74035dd579e02f996ef709510e70facd2ce8b02591398480cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RecidivismVisualizer::Application.config.session_store :cookie_store, key: '_recidivism_visualizer_session'
